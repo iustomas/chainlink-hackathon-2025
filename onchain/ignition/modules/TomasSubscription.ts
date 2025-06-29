67ae284d9b4cb64954a463e044ffb601fc753a4a
TesslaRay <cristian.valdivia@alumnos.usm.cl>
2025-06-29 16:56:26 -0400

    (onchain): deploy contract for tomas subscription

--- onchain/ignition/modules/TomasSubscription.ts
+++ onchain/ignition/modules/TomasSubscription.ts
@@ -5,9 +5,12 @@ export default buildModule("TomasSubscription", (m) => {
   const ETH_USD_PRICE_FEED_BASE_MAINNET =
     "0x71041dddad3595F9CEd3DcCFBe3D1F4b0a16Bb70";
 
+  const WETH_TOKEN_BASE_MAINNET = "0x4200000000000000000000000000000000000006";
+
   /* ─────────────── Deploy ─────────────── */
   const subscription = m.contract("TomasSubscription", [
     ETH_USD_PRICE_FEED_BASE_MAINNET,
+    WETH_TOKEN_BASE_MAINNET,
   ]);
 
   return { subscription };
